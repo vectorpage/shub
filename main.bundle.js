caafb66c7e189f5e0657e639c23a2a2b81683aab
vectorpage <44744919+vectorpage@users.noreply.github.com>
2018-11-11 13:51:02 +0530

    added registration page

--- main.bundle.js
+++ main.bundle.js
@@ -38,7 +38,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/app.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"text-center\">\n    <app-login></app-login>\n</div>\n"
+module.exports = "<div class=\"text-center\">\n        <router-outlet></router-outlet>\n</div>\n"
 
 /***/ }),
 
@@ -62,7 +62,7 @@ var AppComponent = (function () {
     return AppComponent;
 }());
 AppComponent = __decorate([
-    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["o" /* Component */])({
         selector: 'app-root',
         template: __webpack_require__("../../../../../src/app/app.component.html"),
         styles: [__webpack_require__("../../../../../src/app/app.component.css")]
@@ -81,10 +81,12 @@ AppComponent = __decorate([
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/@angular/platform-browser.es5.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__("../../../forms/@angular/forms.es5.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__footer_footer_component__ = __webpack_require__("../../../../../src/app/footer/footer.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__header_header_component__ = __webpack_require__("../../../../../src/app/header/header.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__login_login_component__ = __webpack_require__("../../../../../src/app/login/login.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_router__ = __webpack_require__("../../../router/@angular/router.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__footer_footer_component__ = __webpack_require__("../../../../../src/app/footer/footer.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__header_header_component__ = __webpack_require__("../../../../../src/app/header/header.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__login_login_component__ = __webpack_require__("../../../../../src/app/login/login.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__registration_registration_component__ = __webpack_require__("../../../../../src/app/registration/registration.component.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -98,25 +100,43 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
+
+
+var appRoutes = [
+    {
+        path: 'register',
+        component: __WEBPACK_IMPORTED_MODULE_8__registration_registration_component__["a" /* RegistrationComponent */]
+    },
+    {
+        path: 'login',
+        component: __WEBPACK_IMPORTED_MODULE_7__login_login_component__["a" /* AppLoginComponent */]
+    },
+    {
+        path: '**',
+        redirectTo: 'login'
+    }
+];
 var AppModule = (function () {
     function AppModule() {
     }
     return AppModule;
 }());
 AppModule = __decorate([
-    Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["L" /* NgModule */])({
+    Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["M" /* NgModule */])({
         declarations: [
-            __WEBPACK_IMPORTED_MODULE_3__app_component__["a" /* AppComponent */],
-            __WEBPACK_IMPORTED_MODULE_5__header_header_component__["a" /* AppHeaderComponent */],
-            __WEBPACK_IMPORTED_MODULE_6__login_login_component__["a" /* AppLoginComponent */],
-            __WEBPACK_IMPORTED_MODULE_4__footer_footer_component__["a" /* AppFooterComponent */]
+            __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */],
+            __WEBPACK_IMPORTED_MODULE_6__header_header_component__["a" /* AppHeaderComponent */],
+            __WEBPACK_IMPORTED_MODULE_7__login_login_component__["a" /* AppLoginComponent */],
+            __WEBPACK_IMPORTED_MODULE_5__footer_footer_component__["a" /* AppFooterComponent */],
+            __WEBPACK_IMPORTED_MODULE_8__registration_registration_component__["a" /* RegistrationComponent */]
         ],
         imports: [
             __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
-            __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */]
+            __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */],
+            __WEBPACK_IMPORTED_MODULE_3__angular_router__["c" /* RouterModule */].forRoot(appRoutes)
         ],
         providers: [],
-        bootstrap: [__WEBPACK_IMPORTED_MODULE_3__app_component__["a" /* AppComponent */]]
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]]
     })
 ], AppModule);
 
@@ -174,7 +194,7 @@ var AppFooterComponent = (function () {
     return AppFooterComponent;
 }());
 AppFooterComponent = __decorate([
-    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["o" /* Component */])({
         selector: 'app-footer',
         template: __webpack_require__("../../../../../src/app/footer/footer.component.html"),
         styles: [__webpack_require__("../../../../../src/app/footer/footer.component.css")]
@@ -236,7 +256,7 @@ var AppHeaderComponent = (function () {
     return AppHeaderComponent;
 }());
 AppHeaderComponent = __decorate([
-    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["o" /* Component */])({
         selector: 'app-header',
         template: __webpack_require__("../../../../../src/app/header/header.component.html"),
         styles: [__webpack_require__("../../../../../src/app/header/header.component.css")]
@@ -256,7 +276,7 @@ exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-b
 
 
 // module
-exports.push([module.i, ".login-form{\r\n    width:30%;\r\n    margin-left: 35%;\r\n    margin-top:10%;\r\n}", ""]);
+exports.push([module.i, ".login-form{\r\n    width:30%;\r\n    vertical-align: middle;\r\n    padding: 3%;\r\n    text-align: center;\r\n    display: inline-block;\r\n    background-color: #f7f7f7;\r\n    border-radius: 4px;\r\n    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.22);\r\n    border: solid 0.5px #c1c1c1;\r\n}\r\n\r\n.logo\r\n{\r\n    width: 134px;  \r\n    height: 30px;  \r\n    font-family: CircularStd;  \r\n    font-size: 24px;  \r\n    font-weight: bold;\r\n    font-style: normal;\r\n    font-stretch: normal;\r\n    line-height: normal;\r\n    letter-spacing: normal;\r\n    color: #0275d8;\r\n    padding:1%;\r\n}\r\n\r\n.text-style-1{\r\n    font-weight: normal;\r\n    font-style: italic;\r\n    color: #ff7d00;  \r\n}", ""]);
 
 // exports
 
@@ -269,7 +289,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/login/login.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"login-form\">\r\n    <p class=\"text-center\">Sign in with Email</p>       \r\n    <div class=\"form-group\">\r\n        <input [(ngModel)]=\"email\" type=\"text\" class=\"form-control\" placeholder=\"Email\" required=\"required\">\r\n    </div>\r\n    <div class=\"form-group\">\r\n        <input [(ngModel)]=\"passcode\" type=\"password\" class=\"form-control\" placeholder=\"Password\" required=\"required\">\r\n    </div>\r\n    <div class=\"form-group\">\r\n        <button (click)=\"loginUser()\" type=\"button\" class=\"btn btn-primary btn-block\">Sign in</button>\r\n    </div>        \r\n    <p class=\"text-center\">New Here?</p>\r\n    <button (click)=\"redirectUser()\" type=\"button\" class=\"btn btn-primary btn-block\" style=\"white-space: normal\">I am looking for support</button>\r\n    <button (click)=\"redirectUser()\" type=\"button\" class=\"btn btn-primary btn-block\" style=\"white-space: normal\">Become a Support worker</button>\r\n</div>\r\n    "
+module.exports = "<p class=\"logo\">Support<span class=\"text-style-1\">Hub</span></p><br><br>\r\n<div class=\"login-form\">\r\n    <p class=\"text-center\">Sign in with Email</p>       \r\n    <div class=\"form-group\">\r\n        <input [(ngModel)]=\"email\" type=\"text\" class=\"form-control\" placeholder=\"Email\" required=\"required\">\r\n    </div>\r\n    <div class=\"form-group\">\r\n        <input [(ngModel)]=\"passcode\" type=\"password\" class=\"form-control\" placeholder=\"Password\" required=\"required\">\r\n    </div>\r\n    <div class=\"form-group\">\r\n        <button (click)=\"loginUser()\" type=\"button\" class=\"btn btn-primary btn-block\">Sign in</button>\r\n    </div>        \r\n    <p class=\"text-center\">New Here?</p>\r\n    <button (click)=\"redirectToReg('p')\" type=\"button\" class=\"btn btn-primary btn-block\" style=\"white-space: normal\">I am looking for support</button>\r\n    <button (click)=\"redirectToReg('s')\" type=\"button\" class=\"btn btn-primary btn-block\" style=\"white-space: normal\">Become a Support worker</button>\r\n</div>\r\n    "
 
 /***/ }),
 
@@ -279,6 +299,7 @@ module.exports = "<div class=\"login-form\">\r\n    <p class=\"text-center\">Sig
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppLoginComponent; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_router__ = __webpack_require__("../../../router/@angular/router.es5.js");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -289,11 +310,12 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 
+
 var AppLoginComponent = (function () {
-    function AppLoginComponent() {
+    function AppLoginComponent(router) {
+        this.router = router;
     }
     AppLoginComponent.prototype.ngOnInit = function () {
-        throw new Error("Method not implemented.");
     };
     AppLoginComponent.prototype.loginUser = function () {
         if (this.email == "admin@gmail.com" && this.passcode == "admin") {
@@ -303,21 +325,104 @@ var AppLoginComponent = (function () {
             alert("Invalid Userid Password");
         }
     };
-    AppLoginComponent.prototype.redirectUser = function () {
-        alert("Redirecting to Registration Page");
+    AppLoginComponent.prototype.redirectToReg = function (typeOfUser) {
+        console.log(typeOfUser);
+        //this.data.currentMessage.subscribe(data => this.userData = typeOfUser)
+        this.router.navigate(['register'], { queryParams: { utype: typeOfUser } });
     };
     return AppLoginComponent;
 }());
 AppLoginComponent = __decorate([
-    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["o" /* Component */])({
         selector: 'app-login',
         template: __webpack_require__("../../../../../src/app/login/login.component.html"),
         styles: [__webpack_require__("../../../../../src/app/login/login.component.css")]
     }),
-    __metadata("design:paramtypes", [])
+    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1__angular_router__["b" /* Router */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1__angular_router__["b" /* Router */]) === "function" && _a || Object])
 ], AppLoginComponent);
 
+var _a;
 //# sourceMappingURL=login.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/registration/registration.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, ".registration-form{\r\n    width:40%;\r\n    padding: 3%;\r\n    text-align: center;\r\n    display: inline-block;\r\n    background-color: #f7f7f7;\r\n    border-radius: 4px;\r\n    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.22);\r\n    border: solid 0.5px #c1c1c1;\r\n}\r\n\r\n.header {\r\n    width: 100%;\r\n    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.18);\r\n    background-color: #ffffff;\r\n    color: #0275d8;\r\n}\r\n\r\n.logo\r\n{\r\n    width: 134px;  \r\n    height: 30px;  \r\n    font-family: CircularStd;  \r\n    font-size: 24px;  \r\n    font-weight: bold;\r\n    font-style: normal;\r\n    font-stretch: normal;\r\n    line-height: normal;\r\n    letter-spacing: normal;\r\n    color: #0275d8;\r\n    padding:1%;\r\n}\r\n\r\n.text-style-1{\r\n    font-weight: normal;\r\n    font-style: italic;\r\n    color: #ff7d00;  \r\n}\r\n\r\n", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/registration/registration.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"header\"><span><h2>Create An Account</h2></span><br/></div>\n<br/><br/>\n<div class=\"registration-form\">\n    <p class=\"text-center\">{{this.utype}}</p>\n    <div class=\"form-group\" align=\"left\">\n        <label>Full Name</label>\n        <input [(ngModel)]=\"fullname\" type=\"text\" class=\"form-control\" placeholder=\"Full Name\">\n    </div>\n    <div class=\"form-group\" align=\"left\">\n        <label>Phone Number</label>\n        <input [(ngModel)]=\"phonenumber\" type=\"number\" class=\"form-control\" placeholder=\"Phone Number\">\n    </div>\n    <div class=\"form-group\" align=\"left\">\n        <label>Address</label>\n        <input [(ngModel)]=\"address\" type=\"text\" class=\"form-control\" placeholder=\"Street Address\">\n    </div>\n    <div class=\"form-group\" align=\"left\">\n        <label>Email</label>\n        <input [(ngModel)]=\"email\" type=\"text\" class=\"form-control\" placeholder=\"Email\">\n    </div>\n    <div class=\"form-group\" align=\"left\">\n        <label>Password</label>\n        <input [(ngModel)]=\"passcode\" type=\"password\" class=\"form-control\" placeholder=\"Password\">\n    </div>\n    <p class=\"text-center\">{{this.ucontact}}</p>\n    <div class=\"form-group\" align=\"left\">\n        <label>Name</label>\n        <input [(ngModel)]=\"ename\" type=\"text\" class=\"form-control\" placeholder=\"Full Name\" required=\"required\">\n    </div>\n    <div class=\"form-group\" align=\"left\">\n        <label>Phone Number</label>\n        <input [(ngModel)]=\"enumber\" type=\"number\" class=\"form-control\" placeholder=\"Phone Number\">\n    </div>\n    <div class=\"form-group\" align=\"left\">\n        <label>Street Address</label>\n        <input [(ngModel)]=\"eaddress\" type=\"text\" class=\"form-control\" placeholder=\"Street Address\">\n    </div>\n    <div class=\"form-group\">\n        <button (click)=\"submitUserData()\" class=\"btn btn-primary btn-block\" [disabled]=\"!eaddress || !enumber || !ename || !passcode || !email || !address || !phonenumber || !fullname\">Sign Up</button>\n    </div>\n    <h5>By signing in you agree with Terms of Service and Privacy Policy</h5>\n    <a class=\"text-center\" routerLink=\"/login\">Already have account Sign In</a>\n</div>\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/registration/registration.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return RegistrationComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_router__ = __webpack_require__("../../../router/@angular/router.es5.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+var RegistrationComponent = (function () {
+    function RegistrationComponent(route) {
+        this.route = route;
+    }
+    RegistrationComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.route.queryParams
+            .subscribe(function (params) {
+            if (params["utype"] == "p") {
+                _this.utype = "I am looking support for";
+                _this.ucontact = "Emergency Contact Details";
+            }
+            else {
+                _this.utype = "I want to become a support worker";
+                _this.ucontact = "Referee Contact Details";
+            }
+        });
+    };
+    RegistrationComponent.prototype.submitUserData = function () {
+        alert('Registration Completed successfully');
+    };
+    return RegistrationComponent;
+}());
+RegistrationComponent = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["o" /* Component */])({
+        selector: 'app-registration',
+        template: __webpack_require__("../../../../../src/app/registration/registration.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/registration/registration.component.css")]
+    }),
+    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1__angular_router__["a" /* ActivatedRoute */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1__angular_router__["a" /* ActivatedRoute */]) === "function" && _a || Object])
+], RegistrationComponent);
+
+var _a;
+//# sourceMappingURL=registration.component.js.map
 
 /***/ }),
 
@@ -352,7 +457,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
-    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_20" /* enableProdMode */])();
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_23" /* enableProdMode */])();
 }
 Object(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */])
     .catch(function (err) { return console.log(err); });
